7cf281aa32cabda02a2d3a0ac30ec7abf5863640
cs445 <79168610+eltohenok@users.noreply.github.com>
2021-08-31 01:42:50 -0700

    Create Assi1q2.js

---
+++ first/Assignment/Assi1q2.js
@@ -0,0 +1,11 @@
+
+let prompt = require('prompt-sync')();
+let age = prompt('Please enter age:');
+    while(age<18){
+        age = prompt('Please enter age:');
+    }
+//using do-while
+    do{
+        age = prompt('Please enter age:');
+     }while(age<18); 
+        
