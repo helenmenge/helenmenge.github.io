c4a2e19301bc2b86065274b70de6832f49a03302
cs445 <79168610+eltohenok@users.noreply.github.com>
2021-09-03 05:48:36 -0700

    Create arrayTests.js

---
+++ first/w1d4Arrays/arrayTests.js
@@ -0,0 +1,99 @@
+"use strict";
+
+/* You need the assert and function name declarations to test in node.  
+Comment these out when you send it to the browser with the index.html mocha setup page.
+*/
+const assert = require("assert");  //always need this with node
+const myExports = require("./arrays.js");  //with node need the name of your file with your functions here
+const maxOfThree = myExports.maxOfThree;  //do this for all of the functions used in the Mocha tests
+const multiply = myExports.multiply;
+const sum = myExports.sum;
+const reverseArrayInPlace =myExport.reverseArrayInPlace;
+const reverseArray = myExport.reverseArray;
+const scoreExams = myExport.scoreExams;
+
+describe("maxOfThree", function () {
+    it("tests 1 2 3", function () {
+        assert.strictEqual(maxOfThree(1, 2, 3), 3);
+    });
+    it("tests 1 3 2", function () {
+        assert.strictEqual(maxOfThree(10, 30, 20), 30);
+    });
+    it("tests 2 1 3", function () {
+        assert.strictEqual(maxOfThree(2, 1, 3), 3);
+    });
+    it("tests 2 3 1", function () {
+        assert.strictEqual(maxOfThree(22, 33, 11), 33);
+    });
+    it("tests 3 2 1", function () {
+        assert.strictEqual(maxOfThree(3, 2, 1), 3);
+    });
+    it("tests 3 1 2", function () {
+        assert.strictEqual(maxOfThree(333, 111, 222), 333);
+    });
+    it("tests -1 -2 -3", function () {
+        assert.strictEqual(maxOfThree(-1, -2, -3), -1);
+    });
+    it("tests -1 -2 -2", function () {
+        assert.strictEqual(maxOfThree(-1, -2, -2), -1);
+    });
+    it("tests 5 5 -1", function () {
+        assert.strictEqual(maxOfThree(5, 5, -1), 5);
+    });
+    it("tests -2 0 -2", function () {
+        assert.strictEqual(maxOfThree(-2, 0, -2), 0);
+    });
+    it("tests 6 6 6", function () {
+        assert.strictEqual(maxOfThree(6, 6, 6), 6);
+    });
+});
+
+
+describe("sum and multiply", function () {
+    it("tests sum of 1 2 3", function () {
+        assert.strictEqual(sum([1, 2, 3]), 6);
+    });
+    it("tests multiply 3 2 10", function () {
+        assert.strictEqual(multiply([3, 2, 10]), 60);
+    });
+    it("tests multiply -3 -2 -10", function () {
+        assert.strictEqual(multiply([-3, -2, -10]), -60);
+    });
+    it("tests sum -3 -2 -10", function () {
+        assert.strictEqual(sum([-3, -2, -10]), -15);
+    });
+});
+
+
+
+describe("findLongestWord", function () {
+    it("tests longest", function () {
+        assert.strictEqual(findLongestWord(["this", "is", "a", "test", "longest"]), 7);
+    });
+    it("tests longest with spaces", function () {
+        assert.strictEqual(findLongestWord(["this", "is", "a word with spaces", "test", "longest"]), 18);
+    });
+    it("tests longest with equal length words", function () {
+        assert.strictEqual(findLongestWord(["is", "is", "is", "is", "is"]), 2);
+    });
+    it("tests longest with some words equal length", function () {
+        assert.strictEqual(findLongestWord(["this", "is", "this", "is", "is"]), 4);
+    });
+});
+
+
+
+describe("reverseArray", function () {
+    it("tests reverseArray odd number elements", function () {
+        assert.deepEqual(reverseArray(["A", "B", "C"]), ["C", "B", "A"]);
+    });
+    it("tests reverseArray even number elements", function () {
+        assert.deepEqual(reverseArray(["A", "B", "C", "D"]), ["D", "C", "B", "A"]);
+    });
+    it("tests reverseArrayInPlace even number elements", function () {
+        assert.deepEqual(reverseArrayInPlace(["A", "B", "C", "D"]), ["D", "C", "B", "A"]);
+    });
+    it("tests reverseArrayInPlace odd number elements", function () {
+        assert.deepEqual(reverseArrayInPlace([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1]);
+    });
+});
