dbf8a294f0c3f217930e4720f6dd95a65ea8aed6
cs445 <79168610+eltohenok@users.noreply.github.com>
2021-09-08 06:50:51 -0700

    Create Assw2d2.js

---
+++ first/object/Assw2d2.js
@@ -0,0 +1,17 @@
+
+  function Accumulator(intialValue,increment){
+    this.initialValue;
+    let accObj={
+        currentValue:curentAcumVal,
+        accumulate(){
+            return this.currentValue+increment;
+        },
+        report(){
+            return this.currentValue;
+        }
+    };
+       
+}
+
+  
+  
